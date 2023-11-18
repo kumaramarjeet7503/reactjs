c44b500d4da84fc08f8b93d8855a0ebca693446d
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-18 11:52:19 +0530

    * Added generic heading component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from "react-router-dom"
 import Slider from "./component/Slider.js"
 import data from "./data/data.json"
 import Offers from "./component/Offers.js"
+import Heading from "./component/Heading.js"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <Slider start={data.banner.start}/>
       <Offers offer={data.offer}/>
+      <Heading text="STAR PRODUCTS"/>
     </Router>
   );
 }
