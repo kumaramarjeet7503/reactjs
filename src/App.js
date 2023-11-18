014a0fe248222453e2e92bf98d15fef8e1601864
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-18 11:54:42 +0530

    * Created star product component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import NavBar from './component/NavBar.js';
 import {BrowserRouter as Router} from "react-router-dom"
 import Slider from "./component/Slider.js"
 import data from "./data/data.json"
+import Offers from "./component/Offers.js"
+import Heading from "./component/Heading.js"
+import StarProduct from "./component/StarProduct.js"
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
       <PreNav />
       <NavBar />
       <Slider start={data.banner.start}/>
+      <Offers offer={data.offer}/>
+      <Heading text="STAR PRODUCTS"/>
+      <StarProduct product={data.starProduct}></StarProduct>
+      <Heading text="HOT ACCESSORIES"/>
     </Router>
   );
 }
