a4a931345eaebe9bb158f57ffae1d6be69d299e1
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-22 21:09:44 +0530

    * Hot accessories component

--- src/component/HotAccessiories.js
+++ src/component/HotAccessiories.js
@@ -2,18 +2,30 @@ import React from 'react'
 import HotIndexCard from './HotIndexCard.js'
 import '../styles/hotAccess.css'
 
-function HotAccessiories({music,musicCover}) {
+function HotAccessiories({music,musicCover,device,deviceCover,home,homeCover,lifestyle,lifestyleCover,mobileAccessories,mobileAccessoriesCover}) {
   return (
     <div className='hotAccess'>
         <div>
-            <img src={musicCover}></img>
+            <img src={musicCover ||  musicCover ||  deviceCover || homeCover ||  lifestyleCover ||  mobileAccessoriesCover }></img>
         </div>
         <div>
-            {
-                music.map((item,index)=>{
+            
+                {music && music.map((item,index)=>{
                   return   <HotIndexCard key={item.image}  name={item.name} price={item.price} image={item.image} index={index}  ></HotIndexCard>
-                })
-            }
+                }) }
+                { device && device.map((item,index)=>{
+                  return   <HotIndexCard key={item.image}  name={item.name} price={item.price} image={item.image} index={index}  ></HotIndexCard>
+                }) }
+                {home && home.map((item,index)=>{
+                  return   <HotIndexCard key={item.image}  name={item.name} price={item.price} image={item.image} index={index}  ></HotIndexCard>
+                }) }
+                { lifestyle && lifestyle.map((item,index)=>{
+                  return   <HotIndexCard key={item.image}  name={item.name} price={item.price} image={item.image} index={index}  ></HotIndexCard>
+                }) }
+                { mobileAccessories && mobileAccessories.map((item,index)=>{
+                  return   <HotIndexCard key={item.image}  name={item.name} price={item.price} image={item.image} index={index}  ></HotIndexCard>
+                }) }
+            
         </div>
 
     </div>
